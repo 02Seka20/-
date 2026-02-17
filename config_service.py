f39d20c79bfe0719fe4163c3a698f862ba6b11ba
02Seka20 <smabrosimov@mail.ru>
2026-02-17 14:33:11 +0300

    Fix field name for analysis service

--- config_service.py
+++ config_service.py
@@ -7,30 +7,20 @@ app = FastAPI()
 
 ANALYSIS_URL = "https://pc-microservices.onrender.com/analyze"
 
-# -----------------------------
-# Модель входных данных
-# -----------------------------
 class ConfigurationRequest(BaseModel):
     component_ids: List[int]
 
-# -----------------------------
-# Создать конфигурацию
-# -----------------------------
 @app.post("/configurations")
 def create_configuration(config: ConfigurationRequest):
     try:
-        # Отправляем POST в analysis_service
         response = requests.post(
             ANALYSIS_URL,
-            json={"component_ids": config.component_ids}
+            json={"components": config.component_ids}  # <--- важно
         )
         return response.json()
     except Exception as e:
         return {"error": f"Analysis service unavailable: {str(e)}"}
 
-# -----------------------------
-# Проверка работы
-# -----------------------------
 @app.get("/")
 def root():
     return {"message": "Configuration Service is running"}
